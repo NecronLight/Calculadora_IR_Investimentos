0e2479e1c4cf9b67db6a454ed3f1768e90732d3b
Lucas <lucasluz13@hotmail.com>
2024-10-01 18:26:02 -0300

    Adicionado comentários principais

--- app_investimentos.py
+++ app_investimentos.py
@@ -1,3 +1,4 @@
+#Pede para que o usuário informe um tipo de investimento entre as opções apresentadas
 tipo_investimento = int(input("Informe o número correspondente ao tipo de investimento (1- CDB, 2- LCI, 3- LCA): "))
 
 if tipo_investimento == 1:
@@ -13,25 +14,28 @@ else:
     print("Investimento inválido")
     exit()
 
+#Pede para que o usuário informe o valor a ser resgatado.
 while True:
     try:
-        valor_resgate = float(input("Informe o valor em reais a ser resgatado R$: "))
+        valor_resgate = float(input("Informe o valor em reais a ser resgatado R$: ")) #Converte o número em float
         print("Valor a ser resgatado R$ {:.2f}: ".format(valor_resgate))
         break
 
-    except ValueError:
+
+    except ValueError: #Caso o usuário não informe um valor que possa ser convertido em float, será informado que o valor é inválido
         print("Valor inválido. Insira o valor em reais. Exemplo: 150 ou 150.00")
 
+#Pede para que o usuário informe o valor a ser resgatado.
 while True:
     try:
         dias_investido = int(input("Agora informe o número de dias em que o valor ficou investido: "))
         print("O número de dias em que o valor ficou investido é:", dias_investido,"dias")
         break
 
-    except ValueError:
+    except ValueError: #Caso o usuário não informe um valor 'inteiro', será informado que o valor é inválido
         print("Quantidade de dias informada incorreta. Informe um número inteiro por favor")
 
-
+#Resultado informando a alíquota aplicada e valor a ser pago como imposto com base no número de dias de investimento
 if dias_investido <= 180:
     print("Alíquota de 22,5% de IR")
     print("")
